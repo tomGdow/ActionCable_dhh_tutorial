4ec9ab68be1acd04aad41f744d2ae9ae1e3e5ca7
tomgdow <thomasgdowling@gmail.com>
2016-09-12 21:16:37 +0000

    first commit of code

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root to: 'rooms#show'
+  get 'rooms/show'
+
+  mount ActionCable.server => '/cable'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
